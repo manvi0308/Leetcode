32aff5bd71135c00e20249b00f25907bbd2d04b6
Manvi Chaddha <60390722+manvi0308@users.noreply.github.com>
2023-08-05 07:35:58 +0000

    Added comments related to intution

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -1,5 +1,10 @@
 public class MajorityElement {
     static int majorityElement(int[] nums){
+        //^ Simple application of Boyer's moore algo.
+        //^ Intution is that on traversing the entire array, the ME frequency
+        //^ will always be greater than 0, since it occurs N/2 times. So linearly
+        //^ traversing the array, if current element is the candidate, increase freq, else decrease freq.
+        //^ if at any point of time freq becomes 0, this means there has to be a diff ME.
         int count = 0;
         int candidate = nums[0];
         for(int num : nums){
@@ -13,7 +18,12 @@ public class MajorityElement {
                 count--;
             }
         }
-
+        //~ if the candidate with max votes is a majority element or not.
+        //~ This if statement need to be add only if its explicitly mentioned
+        //~ that the majority element may or may not exists.
+        /* if(count >= nums.length/2){
+            return candidate;
+        }*/
         return candidate;
     }
 }
--- Arrays/RemoveElement.java
+++ Arrays/RemoveElement.java
@@ -1,4 +1,17 @@
 public class RemoveElement {
+    //? 27. Remove Element
+
+    //^ Intution is since we have to place the elements in place in the start 
+    //^ of array, where none of the element should be equal to val. Or in simple
+    //^ terms, the val elements should be placed at end. 
+
+    //& start = 0, end = n - 1
+    //& If a[start] != val, so it should be as it is and start ++
+    //& If a[end] == val, then also its the desired order, end--
+
+    //* Other cases
+    //* a[start] == val || a[end] != val, in any of these two cases we have to swap starting
+    //* and ending index.
     static int removeElement(int[] nums, int val){
         int start = 0;
         int end = nums.length - 1;
