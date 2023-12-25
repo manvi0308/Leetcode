70f0b12c221e6a8873b868c3e31cfaebd30003bd
MChaddha <147035815+MChaddha@users.noreply.github.com>
2023-12-25 19:49:42 +0530

    Added Lowest Common Ancestor, Longest Increasing subsequence using memoization.

---
+++ Trees/LCA.java
@@ -0,0 +1,17 @@
+public class LCA{
+    static TreeNode lca(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null || root== p || root == q){
+        return  root;
+        }
+
+        TreeNode left = lca(root.left, p, q);
+        TreeNode right = lca(root.right, p, q);
+
+        if(left == null)
+            return  right;
+        else if(right == null)
+            return left;
+        else
+            return  root;
+    }
+}
